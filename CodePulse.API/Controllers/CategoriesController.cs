3885ffdc77cdd9dfc1ef03282b99cb48434d0310
VamshiRagipani <vamshirajragipani@gmail.com>
2025-11-08 13:46:24 +0530

    Initial commit - CodePulse API project

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,139 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        // POST: api/categories
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDTO request)
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        // GET: api/categories
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await categoryRepository.GetAllAsync();
+
+            var response = new List<CategoryDto>();
+            foreach (var category in categories)
+            {
+                response.Add(new CategoryDto
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle
+                });
+            }
+
+            return Ok(response);
+        }
+
+        // GET: api/categories/{id}
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> GetCategory(Guid id)
+        {
+            var category = await categoryRepository.GetById(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        // PUT: api/categories/{id}
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> UpdateCategory(Guid id, UpdateCategoryRequestDto request)
+        {
+            var existingCategory = await categoryRepository.GetById(id);
+
+            if (existingCategory == null)
+            {
+                return NotFound();
+            }
+
+            existingCategory.Name = request.Name;
+            existingCategory.UrlHandle = request.UrlHandle;
+
+            var updatedCategory = await categoryRepository.UpdateAsync(existingCategory);
+
+            if (updatedCategory == null)
+            {
+                // Handle case where update failed (e.g., concurrency issue)
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to update category.");
+            }
+
+            var response = new CategoryDto
+            {
+                Id = updatedCategory.Id,
+                Name = updatedCategory.Name,
+                UrlHandle = updatedCategory.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        // DELETE: api/categories/{id}
+        [HttpDelete("{id:Guid}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var deletedCategory = await categoryRepository.DeleteAsync(id);
+
+            if (deletedCategory == null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = deletedCategory.Id,
+                Name = deletedCategory.Name,
+                UrlHandle = deletedCategory.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
